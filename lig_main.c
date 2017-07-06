13e8e82dacc67644f46b623e216686d16069647a
Eva Zou <zouyuan_cf@hotmail.com>
2017-07-06 16:12:21 +0800

    Add lig_main.c file

---
+++ lig_main.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : lig_main.c
+ Author      : zouyuan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	
+	printf("Liguo Hello Demo : GitHub Study, Version 1.0.0 \n");
+	
+	return 0;
+}
+
+
